f2d69ce0469cb86981fce4de7747964a1eeb4246
Naziru Abdussalam Ibrahim <naziruabdussalami@gmail.com>
2023-11-21 19:25:33 +0100

    Udate file

--- sqrt_calculator.py
+++ sqrt_calculator.py
@@ -1,4 +1,16 @@
-def sqrt(num):
-    return num * num
+# importing inbuilt math method
+import math
 
-print(sqrt(7))
+# function that take an argument of a number
+def square_root(number):
+    result = math.sqrt(number)
+    return result
+
+# Input number for which you want to find the square root
+number = float(input("Enter a number: "))
+
+if number < 0:
+    print("Square root of negative numbers is undefined.")
+else:
+    sqrt = square_root(number)
+    print(f"The square root of {number} is equal to: {sqrt}")
